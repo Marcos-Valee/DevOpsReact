d0dee34361d9961d720bcb68e2a584e5f26e7e1e
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-27 12:22:16 -0300

    Adicionando mais informações ao footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ function App() {
   return (
     <>
       <h1>Arquivo pronto para uso!</h1>
-      <footer>Este é o footer da aplicação!</footer>
+      <footer>
+        Este é o footer da aplicação! <br />
+        Aqui você pode colocar políticas de privacidade, informações de contato e muito mais!
+      </footer>
     </>
   )
 }
