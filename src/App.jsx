77a41e1942a63c1d04bc468cce3a994afd6eab49
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-27 16:45:20 -0300

    Primeiro card

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
+import Card from "./MyComponents/card"
+
 function App() {
   return (
     <>
       <header className="bg-blue-500 text-3xl text-white text-center p-6"><h1>Estudos de React</h1></header>
-      <div>
-        A card
-      </div>
+      <Card />
     </>
   )
 }
