1586713f86e44256540d827fc69f149a78ca0052
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-27 16:47:39 -0300

    Alterando textos

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ function App() {
   return (
     <>
       <header><h1>Estudos de React</h1></header>
-      <h1>Arquivo pronto para uso!</h1>
-      <footer>
-        Este é o footer da aplicação! <br />
-        Aqui você pode colocar políticas de privacidade, informações de contato e muito mais!
-      </footer>
     </>
   )
 }
