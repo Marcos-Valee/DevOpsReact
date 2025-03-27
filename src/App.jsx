8b623956f6f766694ba6da4891b095dbd8a60dec
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-27 16:48:12 -0300

    Alteração no header

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./MyComponents/card"
 function App() {
   return (
     <>
-      <header className="bg-blue-500 text-3xl text-white text-center p-6"><h1>Estudos de React</h1></header>
+      <header><h1>Estudos de React</h1></header>
       <Card />
       <Card 
       nome = "Marcos"
