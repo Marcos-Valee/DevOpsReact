d3cc19e8f12e8c317644d43439b2172f270caa23
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-27 16:46:12 -0300

    Segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
     <>
       <header className="bg-blue-500 text-3xl text-white text-center p-6"><h1>Estudos de React</h1></header>
       <Card />
+      <Card 
+      nome = "Marcos"
+      idade = {20}
+      statusEmprego = {true}
+      />
     </>
   )
 }
