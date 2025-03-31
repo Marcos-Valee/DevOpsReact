78050075de531ad3b5dae6ee740cfb940b9d08b8
Marcos-Valee <marcosvini1009@gmail.com>
2025-03-31 16:35:24 -0300

    Correção de erros na main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <>
     <header className="bg-blue-500 text-3xl text-white text-center p-6"><h1>Estudos de React</h1></header>
-    <cards
       <Card />
       <Card 
       nome = "Marcos"
