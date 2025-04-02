ecd72f1a9a685b25e4fb43c0dda99da4e65c11c5
Marcos-Valee <marcosvini1009@gmail.com>
2025-04-02 11:32:44 -0300

    Mudanças no css e deixando o código mais limpo

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,20 @@
-import Card from "./MyComponents/card"
+import Card from "./MyComponents/card";
+import "./index.css";
 
 function App() {
   return (
     <>
-    <header className="bg-blue-500 text-3xl text-white text-center p-6"><h1>Estudos de React</h1></header>
-      <Card />
-      <Card 
-      nome = "Marcos"
-      idade = {20}
-      statusEmprego = {true}
-      />
-      <Card 
-      nome = "Vinicius"
-      idade = {21}
-      statusEmprego = {true}
-      />
-      <Card 
-      nome = "Roberto"
-      idade = {31}
-      statusEmprego = {false}
-      />
-
-    <p>Isto é apenas um teste!</p>
-
+      <header>
+        <h1>Estudos de React</h1>
+      </header>
+      <div className="cards-container">
+        <Card />
+        <Card nome="Marcos" idade={20} statusEmprego={true} />
+        <Card nome="Vinicius" idade={21} statusEmprego={true} />
+        <Card nome="Roberto" idade={31} statusEmprego={false} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/MyComponents/card.jsx
+++ src/MyComponents/card.jsx
@@ -1,6 +1,8 @@
+import "../index.css"
+
 function Card({nome = "não informado",  idade = "não informado", statusEmprego = false}){
     return(
-        <div className="flex flex-col rounded-md border w-50 gap-2 p-3 mt-3 ml-2">
+        <div className="card">
             <span>Nome: {nome}</span>
             <span>Idade: {idade}</span>
             <span>{statusEmprego ? "Empregado" : "Desempregado"}</span>
