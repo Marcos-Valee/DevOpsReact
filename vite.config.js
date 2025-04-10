063caa1b369fcaed395a1219c733c670bbbe6847
Marcos-Valee <marcosvini1009@gmail.com>
2025-04-10 14:53:59 -0300

    Adição do dockerfile na main

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "/DevOpsReact/",
+//   plugins: [react()],
+// });
+
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/DevOpsReact/",
+  base: './', // ou simplesmente remova essa linha
   plugins: [react()],
 });
+
